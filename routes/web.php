83fa3596b30e7673394b19be04d95a10e2bce985
Marco Antonio <marco.antonio.rocha.nunes@hotmail.com>
2024-08-25 10:56:48 -0300

    50%

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\RegistroController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [RegistroController::class, 'index']);
